af6e86e1a0d0e13fd2042f7d143776389dfcdea1
Dayanand21 <146654194+Dayanand21@users.noreply.github.com>
2024-09-06 20:25:37 +0530

    Add files via upload

---
+++ ParkingTicket.java
@@ -0,0 +1,63 @@
+ import java.time.format.DateTimeFormatter;
+
+public class ParkingTicket{
+
+	//public String ParkingTime;
+	public String enterDate;
+	public String enterTime;
+	private long CardNumber ;
+	public String CardType;
+	public int SpotNumber;
+
+	public Car car;
+
+	public Car getAssignedCar(){
+		return car;
+	}
+
+	public void setAssignedCar(Car car){
+		this.car = car;
+	}
+
+	public String getDate(){
+		return enterDate;
+	}
+
+	public void setDate(String enterDate){
+		this.enterDate = enterDate;
+	}
+
+	public String getTime(){
+		return enterTime;
+	}
+
+	public void setTime(String enterTime){
+		this.enterTime = enterTime;
+	}
+
+	private long getCardNumber(){
+		return CardNumber;
+	}
+
+	public void setCardNumber(long CardNumber){
+		this.CardNumber = CardNumber;
+	}
+
+	private String getCardType(){
+		return CardType;
+	}
+
+	public void setCardType(String CardType){
+		this.CardType = CardType;
+	}
+
+
+	public int getSpotNumber(){
+		return SpotNumber;
+	}
+
+	public void setSpotNumber(int SpotNumber){
+		this.SpotNumber = SpotNumber;
+	}
+
+}
---
+++ RandomInfo.java
@@ -0,0 +1,68 @@
+ import java.util.Random;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class RandomInfo{
+	Random rand = new Random();
+
+	String[] states = {"MH", "GJ", "RJ", "DL", "MP", "UP", "KA", "JK", "LA"};
+	String[] dist = {"02", "27", "12", "19", "22", "08", "05", "26", "30"};
+	String[] alpha = {"AB", "CV", "RT", "ZX", "WE", "JK", "RL", "AQ", "PO", "DH"};
+	String[] color = {"Red", "Yellow", "Green", "white", "Brown", "Violet", "Pink"};
+	String[] type = {"Sedan", "van", "Minivan", "Bus", "Pickup-truck", "Hatchback"};
+
+	public String Numberplate(){
+		int st = rand.nextInt(states.length);
+		int di = rand.nextInt(dist.length);
+		int al = rand.nextInt(alpha.length);
+
+		return states[st]+"-"+dist[di]+" "+alpha[al]+" "+rand.nextInt((99 - 10)+ 1) + 10;
+	}
+
+	public String CarColor(){
+		int res = rand.nextInt(color.length);
+		return color[res];
+	}
+
+	public String CarType() {
+		int typ = rand.nextInt(type.length);
+		return type[typ];
+	}
+
+
+	// information for parkingaticket
+	 public String Time(){
+		LocalTime localTime = LocalTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+		String enterTime = localTime.format(dateTimeFormatter);
+		return enterTime;
+	}
+
+	public String ExitTime(){
+		LocalTime localTime = LocalTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+		String exitTime = localTime.format(dateTimeFormatter);
+		return exitTime;
+	}
+
+	public String Date(){
+		LocalDate mydate = LocalDate.now();
+		return mydate.toString();
+	}
+	public String ExitDate(){
+		LocalDate date = LocalDate.now();
+		return date.toString();
+	}
+
+	String[] cardtype = {"Debit", "Credit"};
+	public String CardType(){
+		int caty = rand.nextInt(cardtype.length);
+		return cardtype[caty];
+	}
+
+	public long CardNumber(){
+		return ((rand.nextLong() % 100000000000000L) + 5200000000000000L);
+	}
+
+ }
